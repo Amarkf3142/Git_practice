a02cb3a496f06ea3c176a28bfbd8b0c279ad2a88
amar <fundipalleamar@gmail.com>
2023-12-06 22:33:24 +0530

    Git Commands

---
+++ Git-commands.sh
@@ -0,0 +1,39 @@
+# Configure User Information
+git config --global user.name "Your Name"
+git config --global user.email "your.email@example.com"
+
+# View Configuration
+git config --list
+
+# Initialize a New Repository
+git init
+
+# Clone a Repository
+git clone repository_url
+
+# Check Status
+git status
+
+# Stage Changes
+git add file_name
+
+# Commit Changes
+git commit -m "Commit message"
+
+# Create a New Branch
+git branch branch_name
+
+# Switch to a Branch
+git checkout branch_name
+
+# Create and Switch to a New Branch
+git checkout -b new_branch_name
+
+# Merge Changes
+git merge branch_name
+
+# Add a Remote Repository
+git remote add remote_name repository_url
+
+# Fetch Changes from a Remote
+git fe
